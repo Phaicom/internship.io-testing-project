8eceb60599f376a82e9b5d9f0a2d7a47da77a235
phaicom <reawpai.chun@yahoo.com>
2017-03-08 14:17:07 +0700

    add readLinePalindrome

---
+++ 1/readLinePalindrome.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let fs = require('fs');
+let path = process.argv[2];
+let file = fs.readFileSync(path);
+let count = 0;
+file.toString().split("\n").forEach(i => {
+    let item = i.replace(/[^A-Za-z0-9]+/g, '').split(''),
+    if (before === after)
+        count++;console.log("tests")
+})
+console.log(count)
